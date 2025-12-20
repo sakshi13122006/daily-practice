cda29a6a565e908a5507311cf1887431c4cda583
Sakshi <sakshighogare1312@gmail.com>
2025-12-20 23:55:53 +0530

    second assignment

--- main.py
+++ main.py
@@ -1,1 +1,62 @@
-print("hello world!!!")
+"""
+    for i in range(5):
+            print("Hello World!!!!!")
+
+"""
+
+# name = "Piu"
+# for ch in name:
+#      print(ch)
+
+# for i in range(1, 6):
+#            print(i)
+
+# fruits = ["Apple", "Orange", "Cherry"]
+# for x in fruits:
+#     print(x)
+
+# for x in "Sakshi":
+#     print(x)
+
+# fruits = ["cherry","Strawberry","banana","Mango"]
+# for x in fruits:
+#     print(x)
+#     if x == "banana":
+#        break
+
+# flowers = ["rose","sunflower","Lotus","Tulip"]
+# for x in flowers:
+#      if x == "Lotus":
+#          continue
+#      print(x)
+         
+# for i in range(6):
+#     print(i)
+# else:
+#     print("finally finished")
+
+
+# list = ["apple", "banana","orange"]
+# list.append("cherry")
+# print(list)
+
+list = ["apple","banana","orange","cherry"]
+list.insert(1,"kiwi")
+list.sort()
+list.remove("banana")
+list[1] = "strawberry"
+list.extend(["banana","papaya","guava"])
+print(list)
+
+
+# creating tuple
+# t1 = (10,20)
+# t2 = (30,40,50)
+# t3 = t1 + t2
+# print(t3)
+
+# t = (10,20,30,40,50)
+# if 20  in t:
+#     print("element found")
+
+
